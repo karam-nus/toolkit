eec52d6bb9f9d8cf3f8c9631025e36fb4d4e8633
Karamjot Singh <karamjotsing@me.com>
2025-05-27 22:16:06 +0800

    [Toolkit] Initial commit; add subpackages

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="toolkit",
+    version="0.1.0",
+    packages=find_packages(include=["toolkit", "toolkit.*"]),
+    install_requires=[
+        # Add your package dependencies here, e.g. 'numpy>=1.18.0'
+    ],
+    author="Karamjot Singh",
+    author_email="",
+    description="Quick model evaluation and visualization tools.",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
