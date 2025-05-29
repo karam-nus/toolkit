ef86ed2e71719efa6a70d5c2a40d76d98111a1e2
Karamjot Singh <karamjotsing@me.com>
2025-05-29 22:47:58 +0800

    [Toolkit] Clean installation script

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     url="",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
